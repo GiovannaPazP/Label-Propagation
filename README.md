270db9ad839c91c12680bf633496dde23bc6f863
Giovanna <giovannapaz.16@gmail.com>
2026-08-24 08:56:03 -0300

    Inclusão de função de plot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Label-Propagation
 Algoritmo de detecção de comunidades em redes baseado no algoritmo de label propagation.
+
+# Problemas
+automatização para leitura, rede1 e rede2 começam em 0 mas o zachary começa em 1
+
+rede2 é mais dificil de dividir as partições, mais instavel
--- labelPropagation.py
+++ labelPropagation.py
@@ -1,10 +1,16 @@
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 
 # ================ CONSTANTES ================
 MAXITE = 10
 # ================ ---------- ================
 
+'''
+rede1_duas_comunidades.csv
+rede2.csv
+zachary.csv
+'''
 G = nx.read_edgelist("rede1_duas_comunidades.csv", delimiter=',', nodetype=int)
 
 #print(G.nodes())
@@ -14,7 +20,7 @@ def calcula_moda(valores):
     numeros, contagem = np.unique(valores, return_counts=True) #retorna valores unicos e quantas vezes aparece
     modas = numeros[contagem == contagem.max()] #filtra os numeros por aqueles com maior repetiçaõ
     moda = np.random.choice(modas)
-    print("moda:", moda, ",", contagem, ",", contagem.max()) #tirar dps
+    #print("moda:", moda, ",", contagem, ",", contagem.max()) #tirar dps
     return moda
 
 def labelPropagation(G, MAXITE):
@@ -26,20 +32,21 @@ def labelPropagation(G, MAXITE):
 
     for iteracao in range(MAXITE):
         if not MUDOU:
+            print("Finalização por Imutabilidade")
             break ## ou seja, não mudou
 
         MUDOU = False
         ordem_vertices = np.random.permutation(n)
-        print(ordem_vertices)
+        #print(ordem_vertices)
 
         for i in ordem_vertices:
-            vizinhos = list(G.neighbors(i))
+            vizinhos = list(G.neighbors(i)) #nao retorna None, retorna lista []
 
             if len(vizinhos) > 0:
-                print(vizinhos)
+                #print(vizinhos)
                 rotulos_vizinhos = rotulos[vizinhos] #vizinhos é o indice, pega os valores de rotulos correspondente a essa lista de indice -- só numpy
-                print(rotulos_vizinhos)
-                print()
+                #print(rotulos_vizinhos)
+                #print()
                 novo_rotulo = calcula_moda(rotulos_vizinhos)
                 if novo_rotulo != rotulos[i]:
                     rotulos[i] = novo_rotulo
@@ -48,10 +55,26 @@ def labelPropagation(G, MAXITE):
     print("rotulos:", rotulos)
     return rotulos
 
+def plot(G, rotulos):
+    pos = nx.spring_layout(G, seed=42) #para plotar grafos, fixa a seed para não ficar mudando a posição
+    cores = rotulos
+
+    nx.draw(
+        G, pos,
+        node_color = cores,
+        cmap = plt.cm.tab10,
+        with_labels = True,
+        node_size = 500,
+        font_color = "black"
+    )
+
+    plt.title("Comunidades Detectadas")
+    plt.show()
 
 def main():
-    np.random.seed(42)
+    #np.random.seed(42)
     print(sorted(G.nodes()))
-    labelPropagation(G, MAXITE)
-
+    rotulos = labelPropagation(G, MAXITE)
+    plot(G, rotulos)
 main()
+
